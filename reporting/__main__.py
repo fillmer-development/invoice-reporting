5b002616f01853d138c48217ab2055fcde2efe91
malte <malte.fillmer@gmail.com>
2021-05-13 08:52:08 +0000

    make executable

--- reporting/__main__.py
+++ reporting/__main__.py
@@ -1,8 +1,31 @@
-from collections import defaultdict
-import csv
-import os
-import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
+import openpyxl
+import os
+import csv
+from configparser import ConfigParser
+
+
+def main():
+    config = ConfigParser()
+    config.read('./config.ini')
+    input = config['PATHS'].get('source_directory')
+    output = config['PATHS'].get('output_path')
+    mapping = dict(config['MAPPING'])
+
+    with open(output, 'w') as out:
+        header = True
+        w = None
+        for item in read_directory(input):
+            row = parse_excel_file(load_excel_file(item), mapping=mapping)
+
+            if not type(row) == dict:
+                continue
+            if header:
+                w = csv.DictWriter(out, row.keys())
+                w.writeheader()
+                header = False
+
+            w.writerow(row)
 
 
 def load_excel_file(file):
